44056b71e8f028f9ab6797d3deefbd86b0b034cf
Artem Savchuk <artemchik1825@gmail.com>
2026-05-31 01:58:40 +0300

    Add PostgreSQL docker setup

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ cd backend
 $env:DB_URL="jdbc:postgresql://localhost:5432/sportstore"
 $env:DB_USERNAME="sportstore"
 $env:DB_PASSWORD="sportstore123"
+$env:SPRING_PROFILES_ACTIVE="postgres"
 mvn spring-boot:run
 ```
 
--- backend/src/main/java/com/sportshop/sport_shop/SportShopApplication.java
+++ backend/src/main/java/com/sportshop/sport_shop/SportShopApplication.java
@@ -3,10 +3,13 @@ package com.sportshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SportShopApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(SportShopApplication.class, args);
 	}
 
